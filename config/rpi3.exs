5f1295f9c7a9cc797b87dd44dac416eaf66b27d1
Ryan Yogan <ryanyogan@gmail.com>
2020-12-31 09:46:23 -0600

    Updated the GPIO pin mapping to the breadboard BCM pins

--- config/rpi3.exs
+++ config/rpi3.exs
@@ -1,5 +1,5 @@
 import Config
 
 config :clock,
-  pins: %{0 => 26, 1 => 13, 2 => 6, 3 => 5, 4 => 16, 5 => 12},
+  pins: %{0 => 21, 1 => 19, 2 => 6, 3 => 23, 4 => 12, 5 => 18},
   led_adapter: Clock.LEDAdapter.Embedded
