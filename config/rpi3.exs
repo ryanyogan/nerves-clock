00586d5053702ddaec4d7b1cdb310ac8c02ff982
Ryan Yogan <ryanyogan@gmail.com>
2020-12-31 10:45:48 -0600

    put a fn in the  config on accident

--- config/rpi3.exs
+++ config/rpi3.exs
@@ -4,4 +4,4 @@ import Config
 config :clock,
   pins: %{0 => 21, 1 => 19, 2 => 6, 3 => 23, 4 => 12, 5 => 18},
   led_adapter: Clock.LEDAdapter.Embedded,
-  send_ticks_fn: fn -> :timer.send_interval(60_000, :tick) end
+  send_ticks_fn: :timer.send_interval(60_000, :tick)
